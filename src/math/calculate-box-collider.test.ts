94f1983524fe732b84f5d3dac3fab35bb2c437b3
Storm <stormmuller2@gmail.com>
2025-02-26 22:03:52 +0000

    fix: tests

--- src/math/calculate-box-collider.test.ts
+++ src/math/calculate-box-collider.test.ts
@@ -1,20 +1,19 @@
 import { describe, expect, it } from 'vitest';
-import { Path } from '../common/path/path';
 import { calculateBoxCollider } from './calculate-box-collider';
 import { Vector2 } from './vector2';
 
 describe('calculateBoxCollider', () => {
   it('should throw an error if the path has less than 2 points', () => {
-    const path = new Path([new Vector2(0, 0)]);
+    const points = [new Vector2(0, 0)];
 
-    expect(() => calculateBoxCollider(path)).toThrow(
+    expect(() => calculateBoxCollider(points)).toThrow(
       'There needs to be at least 2 points in order to calculate a bounding box',
     );
   });
 
   it('should correctly calculate the bounding box for a simple path', () => {
-    const path = new Path([new Vector2(0, 0), new Vector2(1, 1)]);
-    const boxCollider = calculateBoxCollider(path);
+    const points = [new Vector2(0, 0), new Vector2(1, 1)];
+    const boxCollider = calculateBoxCollider(points);
 
     expect(boxCollider.minX).toBe(0);
     expect(boxCollider.maxX).toBe(1);
@@ -23,13 +22,13 @@ describe('calculateBoxCollider', () => {
   });
 
   it('should correctly calculate the bounding box for a complex path', () => {
-    const path = new Path([
+    const points = [
       new Vector2(0, 0),
       new Vector2(1, 1),
       new Vector2(-1, -1),
       new Vector2(2, 2),
-    ]);
-    const boxCollider = calculateBoxCollider(path);
+    ];
+    const boxCollider = calculateBoxCollider(points);
 
     expect(boxCollider.minX).toBe(-1);
     expect(boxCollider.maxX).toBe(2);
@@ -38,8 +37,8 @@ describe('calculateBoxCollider', () => {
   });
 
   it('should correctly calculate the bounding box for a vertical line', () => {
-    const path = new Path([new Vector2(0, 0), new Vector2(0, 2)]);
-    const boxCollider = calculateBoxCollider(path);
+    const points = [new Vector2(0, 0), new Vector2(0, 2)];
+    const boxCollider = calculateBoxCollider(points);
 
     expect(boxCollider.minX).toBe(0);
     expect(boxCollider.maxX).toBe(0);
@@ -48,8 +47,8 @@ describe('calculateBoxCollider', () => {
   });
 
   it('should correctly calculate the bounding box for a horizontal line', () => {
-    const path = new Path([new Vector2(0, 0), new Vector2(2, 0)]);
-    const boxCollider = calculateBoxCollider(path);
+    const points = [new Vector2(0, 0), new Vector2(2, 0)];
+    const boxCollider = calculateBoxCollider(points);
 
     expect(boxCollider.minX).toBe(0);
     expect(boxCollider.maxX).toBe(2);
