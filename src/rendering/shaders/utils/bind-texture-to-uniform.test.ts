e7a3a85455dc11a0a035f028745b08dd27d183d5
Storm <stormmuller2@gmail.com>
2025-03-12 22:19:20 +0200

    test: update bindTextureToUniform tests to remove program dependency and improve clarity

--- src/rendering/shaders/utils/bind-texture-to-uniform.test.ts
+++ src/rendering/shaders/utils/bind-texture-to-uniform.test.ts
@@ -4,8 +4,9 @@ import { bindTextureToUniform } from './bind-texture-to-uniform';
 
 describe('bindTextureToUniform', () => {
   let gl: WebGLRenderingContext;
-  let program: WebGLProgram;
   let texture: WebGLTexture;
+
+  const uniformLocation = {} as WebGLUniformLocation;
 
   beforeEach(() => {
     gl = {
@@ -17,42 +18,26 @@ describe('bindTextureToUniform', () => {
       uniform1i: vi.fn(),
     } as unknown as WebGLRenderingContext;
 
-    program = {} as WebGLProgram;
     texture = {} as WebGLTexture;
   });
 
   it('should bind the texture to the specified texture unit and uniform', () => {
-    const uniformName = 'u_texture';
     const textureUnit = 0;
-    const uniformLocation = {} as WebGLUniformLocation;
 
     vi.spyOn(gl, 'getUniformLocation').mockReturnValue(uniformLocation);
 
-    bindTextureToUniform(gl, program, texture, uniformName, textureUnit);
+    bindTextureToUniform(gl, texture, uniformLocation, textureUnit);
 
     expect(gl.activeTexture).toHaveBeenCalledWith(gl.TEXTURE0 + textureUnit);
     expect(gl.bindTexture).toHaveBeenCalledWith(gl.TEXTURE_2D, texture);
-    expect(gl.getUniformLocation).toHaveBeenCalledWith(program, uniformName);
     expect(gl.uniform1i).toHaveBeenCalledWith(uniformLocation, textureUnit);
   });
 
   it('should throw an error if the texture unit is greater than the maximum allowed', () => {
-    const uniformName = 'u_texture';
     const textureUnit = 32;
 
     expect(() => {
-      bindTextureToUniform(gl, program, texture, uniformName, textureUnit);
+      bindTextureToUniform(gl, texture, uniformLocation, textureUnit);
     }).toThrow(`Cannot bind to texture unit greater than 31.`);
-  });
-
-  it('should throw an error if the uniform is not found in the shader', () => {
-    const uniformName = 'u_texture';
-    const textureUnit = 0;
-
-    vi.spyOn(gl, 'getUniformLocation').mockReturnValue(null);
-
-    expect(() => {
-      bindTextureToUniform(gl, program, texture, uniformName, textureUnit);
-    }).toThrow(`Uniform '${uniformName}' not found in the shader.`);
   });
 });
